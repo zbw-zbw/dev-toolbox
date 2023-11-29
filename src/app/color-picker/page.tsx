abe8cc53cd2decf2cf337bedda4f8b5f453b6e1c
zbw01218944 <zbw01218944@alibaba-inc.com>
2023-11-29 18:27:30 +0800

    refactor: ♻️ color-picker

--- src/app/color-picker/page.tsx
+++ src/app/color-picker/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { useState } from 'react';
-
 import { ColorPicker as AntdColorPicker, Space } from 'antd';
 import { Color } from 'antd/es/color-picker';
 
@@ -9,6 +8,7 @@ import CopyBtn from '@/components/copy-btn';
 
 function ColorPicker() {
   const [color, setColor] = useState<Color>();
+  const colorStr = color?.toHexString() || '';
 
   const onColorChange = (value: Color) => {
     setColor(value);
@@ -19,7 +19,7 @@ function ColorPicker() {
       <h2 className="mb-4 text-xl font-bold">颜色选择器：</h2>
       <Space size="large">
         <AntdColorPicker allowClear showText trigger="hover" value={color} onChange={onColorChange} />
-        <CopyBtn value={color?.toHexString() || ''} className="mb-0.5" />
+        <CopyBtn value={colorStr} className="mb-0.5" />
       </Space>
     </div>
   );
